6359bbe57bd587dfe4a1149736241ea6d6d0c2c7
Lucas Leperlier <71217820+dev-Sph1nX@users.noreply.github.com>
2022-11-17 21:55:57 +0100

    footer done

--- components/Footer/footer.tsx
+++ components/Footer/footer.tsx
@@ -1,7 +1,78 @@
 import React from "react";
+import {SOURCES} from './../../public/data/sources';
+
+import LinesEllipsis from 'react-lines-ellipsis';
+import Image from "next/image";
 
 export function Footer(){
-    return <div className="h-[300px]">
-        <i> Footer </i>
+
+    const tier = Math.ceil(SOURCES.length / 3);    
+    const firstTier = SOURCES.slice(0, tier)
+    const secondTier = SOURCES.slice(tier, tier*2)
+    const thirdTier = SOURCES.slice(tier*2)
+
+    return <div className="flex flex-col">
+        <div className="pt-[61px] pb-[76px] px-[70px] bg-[#F7F7F7]">
+            <span className="font-['Eyra-400ExtraLight'] uppercase leading-8">Sources</span>
+            <div className="w-full flex gap-10">
+                <div className="flex flex-col w-1/3">
+                {
+                    firstTier.map((s)=>{
+                        return <Link href={s.link}>{s.label}</Link>                        
+                    })
+                }
+                </div>
+                <div className="flex flex-col w-1/3">
+                {
+                    secondTier.map((s)=>{
+                        return <Link href={s.link}>{s.label}</Link>                        
+                    })
+                }
+                </div>
+                <div className="flex flex-col w-1/3">
+                {
+                    thirdTier.map((s)=>{
+                        return <Link href={s.link}>{s.label}</Link>                        
+                    })
+                }
+                </div>
+            </div>
+        </div>
+        <div className="py-9 px-[70px] bg-[#141414] flex">
+            <div className="flex gap-10">
+                <Image
+                    src="/image/logoCCI.png"
+                    width={200}
+                    height={59}
+                    alt="Logo de la CCI" />
+                <Image
+                    src="/image/logoGobelins.png"
+                    width={60}
+                    height={60}
+                    alt="Logo de Gobelins Paris" />
+            </div>
+            <div className="flex-1 text-center text-[#FEFCFF] font-['Eyra-400Light']">
+                Projet réalisé dans le cadre de la formation CCI - École des Gobelins Annecy par <br />
+                <span className="font-['Eyra-400Regular']">
+                    Lucas Leperlier, Jean Deleage, Apolline Grember, Jereymy Lagadec, Loïc Greco
+                </span>
+            </div>
+            <div className="font-['Eyra-400ExtraLight'] text-[#FEFCFF]">
+                <p>Projet étudiant B3</p>
+                <p>Promotion 2022-2023</p>
+            </div>
+            
+        </div>
     </div>
+}
+
+function Link({href, children}:any): JSX.Element{
+    return <a 
+        href={href} 
+        target="_blank" 
+        style={{textUnderlineOffset: "2px"}}
+        className="whitespace-nowrap overflow-hidden text-ellipsis font-['Eyra-400Medium'] underline"
+    >
+        {children}
+    </a>
 }
---
+++ public/data/sources.js
@@ -0,0 +1,13 @@
+ 
+ export const SOURCES = [
+    {label: "RATP - Mobilité pour tous", link: "https://handicap.paris.fr/documents/2019/11/dp_accessibilite_mai2019_HD-reduit.pdf" },
+    {label: "OCIRP - Les chiffres-clés du handicap en France", link: "https://ocirp.fr/actualites/les-chiffres-cles-du-handicap-en-france" },
+    {label: "INSEE - Le handicap en France", link: "https://www.seton.fr/infographie-handicap-france.html" },
+    {label: "HANDIRECT - Des personnes en fauteuil roulant mais pas seulement", link: "https://handirect.fr/pmr-des-personnes-en-fauteuil-roulant-mais-pas-seulement/" },
+    {label: "AGEFIGH - Les personnes handicapées et l'emploi", link: "https://www.agefiph.fr/sites/default/files/medias/fichiers/2019-09/CHIFFRE-CLES-2018-AGEFIPH-WEB.pdf" },
+    {label: "APUR - Le handicap à Paris, Analyse des tendances récentes", link: "https://www.apur.org/sites/default/files/documents/publication/etudes/oph_handicap_paris_analyse_tendances_recentes.pdf" },
+    {label: "INSEE - Flash Île-de-France n° 60", link: "https://www.insee.fr/fr/statistiques/5425974#:~:text=Un%20Francilien%20sur%20deux%20parcourt,un%20Seine%2Det%2DMarnais" },
+    {label: "OMNIL - Enquête PMR", link: "https://www.omnil.fr/spip.php?article152" },
+
+ ]
+
