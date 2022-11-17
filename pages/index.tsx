2ac03913ef120333cdfeb84f4cb8c47b0bdcc71d
Lucas Leperlier <71217820+dev-Sph1nX@users.noreply.github.com>
2022-11-17 09:02:51 +0100

    add shitty Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import { Header } from '../components/Header/header'
+import { ShittyPage } from '../components/Header/shittyPage'
 
 export default function Home() {
   return (
     <div className='flex flex-col'>
       <Header />
-      {/* content */}
+      <ShittyPage/>
     </div>
   )
 }
