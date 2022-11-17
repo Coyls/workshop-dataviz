8d8d9b84eab6560c9dca9007a309af1248432931
Lucas Leperlier <71217820+dev-Sph1nX@users.noreply.github.com>
2022-11-17 13:24:16 +0100

    fix header sensibility

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -1,28 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { HeaderItem } from '../HeaderItem/headerItem';
 import { Highlighter } from '../Highlighter/hightlighter';
 import { Logo } from '../Logo/logo';
 import styles from './header.module.css';
 import cls from 'classnames';
+import { useScroll } from 'framer-motion';
 
 export function Header() {
 
     const [isDeployed, setIsDeployed] = useState(true);
     const [currentIndex, setCurrentIndex] = useState(1);
+    const { scrollY } = useScroll();
 
-    if ( typeof window !== "undefined") {
-        var prevScrollpos = window.pageYOffset;
-        // browser code
-        window.onscroll = function() {
-            var currentScrollPos = window.pageYOffset;
-            if (prevScrollpos > currentScrollPos) {
-                setIsDeployed(true)
-            } else {
-                setIsDeployed(false)
-            }
-            prevScrollpos = currentScrollPos;
+    function update() {
+        if (scrollY?.get() < scrollY?.getPrevious()) {
+          setIsDeployed(true);
+        } else if (scrollY?.get() > 150 && scrollY?.get() > scrollY?.getPrevious()) {
+          setIsDeployed(false);
         }
       }
+      
+      useEffect(() => {
+        return scrollY.onChange(() => update());
+      });
+
+    // useEffect(() => {
+    //     return scrollY.onChange(() => {
+    //       console.log("current: ", scrollY.get())
+    //       console.log("prev: ", scrollY.getPrevious())
+    //     })
+    //   }, [])
+
+    // if ( typeof window !== "undefined") {
+    //     var prevScrollpos = window.pageYOffset;
+    //     window.onscroll = function() {
+    //         var currentScrollPos = window.pageYOffset;
+    //         if (prevScrollpos > currentScrollPos) {
+    //             setIsDeployed(true)
+    //         } else {
+    //             setIsDeployed(false)
+    //         }
+    //         prevScrollpos = currentScrollPos;
+    //     }
+    //   }
 
     
 
