9d7b009501d93e25c3118b657af5e3dad9b18704
Lucas Leperlier <71217820+dev-Sph1nX@users.noreply.github.com>
2022-11-17 09:28:36 +0100

    fix shitty

--- components/Header/shittyPage.tsx
+++ components/Header/shittyPage.tsx
@@ -9,7 +9,6 @@ export function ShittyPage({}){
           <div className="mx-auto max-w-md px-4 text-center sm:max-w-2xl sm:px-6 lg:flex lg:items-center lg:px-0 lg:text-left">
             <div className="lg:py-24">
               <h1 className="mt-4 text-4xl font-bold tracking-tight text-black sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl"><span className="block text-pink-500">Introducing </span><span className="block text-black">Landing Website</span></h1>
-              <p className="mt-3 text-base text-gray-400 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">Our Website will launch soon. Join the waitlist to try the beta before it's publicly available.</p>
               <div className="mt-10 sm:mt-12">
                   <div className="sm:flex">
                     <div className="min-w-0 flex-1"><label className="sr-only">Email address</label><input id="email" type="email" placeholder="Enter your email" className="block w-full rounded-md border-0 bg-gray-200 px-4 py-3 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-400" /></div>
